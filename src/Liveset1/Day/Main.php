0d178f16d2dd6c062c130fd906cd38ca5289ca3b
Liveset1 <55640842+Liveset1@users.noreply.github.com>
2019-10-13 22:10:38 -0700

    Update Main.php

--- src/Liveset1/Day/Main.php
+++ src/Liveset1/Day/Main.php
@@ -1,33 +1,29 @@
-<?php
+<?php 
 
 namespace Liveset1\Day;
 
 use pocketmine\plugin;
-use pocketmine\plugin\PluginBase;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
-use pocketmine\utils\TextFormat as c;
-use pocketmine\Player;
-use pocketmine\Server;
+use pocketmine\plugin\PluginBase; 
+use pocketmine\command\CommandSender; 
+use pocketmine\command\Command; use pocketmine\utils\TextFormat as c; use pocketmine\Player; 
+use pocketmine\Server; 
 
 class Main extends PluginBase {
 
-    public function  onEnable(){
-        $this->getLogger()->info("Day Enabled");
-    }
+public function  onEnable(){
+$this->getLogger()->info("Day Enabled")
+}  
 
-    public function onDisable(){
-        $this->getLogger()->info("Disabled Day");
-    }
-
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
-       if (strtlower($cmd->getName()) == "day"){
-	  if ($sender->hasPermission("day")){
-		  $sender->sendMessage(c::BOLD.c::DARK_PURPLE."(!)".c::DARK_AQUA."Time set to day");
-	  $player->getServer()->dispatchCommand($player, "time set 0");
-	  }elseIf(!$sender->hasPermission("day")){
-		  $sender->sendMessage(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." Invaild Permission");
-	  }
-    }
-    }
+public function onDisable(){         
+$this->getLogger()->info("Disabled Day");     
+} 
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+      if (strtlower($cmd->getName()) == "day"){
+        if ($sender->hasPermission("day")){
+             $sender->sendMessage(c::BOLD.c::DARK_PURPLE."(!)".c::DARK_AQUA."Time set to day");
+             $player->getServer()->dispatchCommand($player, "time set 0");       }elseIf(!$sender->hasPermission("day")){
+           $sender->sendMessage(c::BOLD.c::DARK_RED."(!)".c::RESET.c::RED." Invaild Permission");
+                       }     
+               }     
+       }
 }
